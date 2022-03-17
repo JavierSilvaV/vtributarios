50cb28f2b83088747a35198cd27938d576392b7d
JavierSilvaV <ja.silva@duocuc.cl>
2022-03-17 16:52:43 -0300

    3.0

--- src/Components/Contacto/Contacto.js
+++ src/Components/Contacto/Contacto.js
@@ -1,96 +1,47 @@
-import React, { useState, useRef } from "react";
-import ReCAPTCHA from "react-google-recaptcha";
+import React from "react";
 import "./Contacto.css";
 import Phone from '@mui/icons-material/PhoneInTalk';
 import Email from '@mui/icons-material/Email';
 import Location from '@mui/icons-material/LocationOn';
+import  Instagram  from '@material-ui/icons/Instagram';
 
 function Contacto() {
-    const recaptchaRef = useRef(null);
-
-    const [status, setStatus] = useState("Enviar");
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if(recaptchaRef.current.getValue()){
-            setStatus("Enviando...");
-            const { name, nro, email, message } = e.target.elements;
-            let details = {
-                name: name.value,
-                nro: nro.value,
-                email: email.value,
-                message: message.value,
-            };
-            let response = await fetch("http://localhost:5000/contact", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json;charset=utf-8",
-                },
-            body: JSON.stringify(details),
-    });
-    
-        setStatus("Enviar");
-        let result = await response.json();
-        alert(result.status);
-        } else {
-            alert('Verifique que no es un robot');
-        }
-};
     return (
         <section className="contactenos">
         <div className="bg-image"></div>
-        <form onSubmit={handleSubmit}>
-        <h1 className="contactenos-titutlo">Contáctenos</h1>
         <div className="wrapper-contacto">
-        <div className="formulario">
-            <div className="nombre">
-                <label htmlFor="name"></label>
-                <input type="text" id="name" required placeholder="Nombre Apellido"/>
-            </div>
-            <div className="nro">
-                <label htmlFor="nro"></label>
-                <input type="text" id="nro" required maxLength={9} placeholder="Télefono"
-                minLength={9}
-                    onKeyPress={(event) => {
-                        if (!/[0-9]/.test(event.key)) {
-                        event.preventDefault();}}}/>
-            </div>
-            <div className="email">
-                <label htmlFor="email"></label>
-                <input type="email" id="email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-                placeholder="ejemplo@email.cl"></input>
-            </div>
-            <div className="mensaje">
-                <label htmlFor="message"></label>
-                <textarea id="message" required placeholder="Mensaje"/>
-            </div>
-            <ReCAPTCHA
-            className="recaptcha"
-            ref={recaptchaRef}
-            sitekey={"6LeysVoeAAAAAJXlm6Zsp8NihNMKNqzRbzIKLXrO"}/>
-            <button type="submit" className="boton">{status}</button>
-        </div>
+        <h1 className="contactenos-titutlo">Contáctenos</h1>
+        <div>
         <div className="informacion">
             <div className="icon-informacion">
-            <Phone className="telefono"></Phone>
+                <Phone className="telefono"></Phone>
+                <h1>Llámanos</h1>
+                <p>(+56 9) 7217 2916</p>
             </div>
-            <h1>Llámanos</h1>
-            <p>(+56 2) 2934 7107</p>
-            <p>(+56 9) 9842 5866</p>
             <div  className="icon-informacion">
-            <Email className="email-icon"></Email>
+                <a href="mailto:c.alvarez@vtributaria.cl"><Email className="email-icon"/></a>
+                <h1>Escríbenos</h1>
+                <a href="mailto:c.alvarez@vtributaria.cl">c.alvarez@vtributaria.cl</a>
             </div>
-            <h1>Escríbenos</h1>
-            <p>contacto@vtriburaria.cl</p>
             <div  className="icon-informacion">
-            <Location className="ubicacion"></Location>
+                <Location className="ubicacion"></Location>
+                <h1>Visítanos</h1>
+                <p>Avenida 2 Sur 870, oficina 818</p>
+                <p>Talca – Región del Maule</p>
+                <p>Chile</p>
             </div>
-            <h1>Visítanos</h1>
-            <p>Avenida 2 Sur 870, oficina 818</p>
-            <p>Talca – Región del Maule</p>
-            <p>Chile</p>
+            <div  className="icon-informacion">
+                
+                <a href="https://www.instagram.com/vtributaria_cl/" target="_blank" rel="noopener noreferrer"><Instagram className="instagram"/> </a>
+
+                <h1>@vtributaria_cl</h1>
+                <p>Haz click en el ícono para ir</p>
+                <p> a nuestra página de Instagram</p>               
+
+            </div>
         </div>
         </div>
-        </form>
+        </div>
     </section>
 
 )
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import "./Footer.css";
 import logo from "../../Assets/LogoV.png";
-import { Facebook } from '@material-ui/icons';
-import { Instagram } from '@material-ui/icons';
-import { LinkedIn } from '@material-ui/icons';
 
 function Footer() {
   return (
@@ -11,29 +8,22 @@ function Footer() {
     <div className="container-footer"></div>
     <footer className="footer-distributed">
     <div className="footer-left">
-      <img
+      <div className="wrapper-logo-f">      <img
     src={logo}
     className="footer-logo"
-    alt="Logo"/>
-
+    alt="Logo"/></div>
   <p className="footer-company-name">V Auditores – Asesoría Contable, Remuneraciones y Financiera</p>
 </div>
 
 <div className="footer-center">
 
   <div>
-    <i className="fa fa-map-marker"></i>
-    <p><span>Avenida 2 Sur 870, oficina 818</span> Talca</p>
   </div>
 
   <div>
-    <i className="fa fa-phone"></i>
-    <p>+56 9 7217 2916</p>
   </div>
 
   <div>
-    <i className="fa fa-envelope"></i>
-    <p><a href="mailto:contacto@spbox.cl">contacto@vtriburaria.cl</a></p>
   </div>
 
 </div>
@@ -41,14 +31,10 @@ function Footer() {
 <div className="footer-right">
 
   <p className="footer-company-about">
-    <span>Redes</span>
-    ¡Estemos conectados!, puedes encontrarnos en Facebook, Instagram o Linkedin.
+    <span></span>
   </p>
 
   <div className="footer-icons">
-    <a href="https://www.facebook.com/spbox.cl" target="_blank" rel="noopener noreferrer"><Facebook style={{fontSize: 35}}/><i className="facebook"></i></a>
-    <a href="https://www.instagram.com/vtributaria_cl/" target="_blank" rel="noopener noreferrer"><Instagram style={{fontSize: 35}}/><i className="instagram"></i></a>
-    <a href="https://www.linkedin.com/company/spbox" target="_blank" rel="noopener noreferrer"><LinkedIn style={{fontSize: 35}}/><i className="linkedin"></i></a>
   </div>
 
 </div>
